168d048bfa8069ab9a22e506905953f7179d2089
samtisi <samtisi@gmail.com>
2015-05-30 00:18:50 -0400

    Checkpoint Alpha

--- 1.R
+++ 1.R
@@ -1,4 +1,27 @@
 setwd("~/GitHub/R-programming-assignment-3")
-outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-outcome[, 11] <- as.numeric(outcome[, 11])
 hist(outcome[, 11])
+best <- function(state, outcome){
+  #options(warn=-1)
+  df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  df[, 11] <- as.numeric(df[, 11])
+  df[, 17] <- as.numeric(df[, 17])
+  df[, 23] <- as.numeric(df[, 23])
+  statedat <- df[df$State==state,]
+  if(length(statedat[,1])==0){
+    stop("invalid state")
+  }
+  if(outcome == "heart attack"){
+    here <- statedat[statedat$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack==min(statedat[,11], na.rm = TRUE),2]
+    here <- here[!is.na(here)]
+  }
+  if(outcome == "heart failure"){
+    here <- statedat[statedat$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure==min(statedat[,17], na.rm = TRUE),2]
+    here <- here[!is.na(here)]
+  }
+  if(outcome == "pneumonia"){
+    here <- statedat[statedat$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia==min(statedat[,23], na.rm = TRUE),2]
+    here <- here[!is.na(here)]
+  }
+  else {stop("invalid outcome")}
+  here
+}
